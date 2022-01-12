4e3f79eba342f6070a1cfa2ed92f28d79de8d88f
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-12 11:53:04 +0200

    feat: add suport for install_and_export_target and enable_ros_tooling_for_target

---
+++ game_engineConfig.cmake
@@ -0,0 +1,14 @@
+include(CMakeFindDependencyMacro)
+
+#find_dependency will correctly forward REQUIRED or QUIET args to the consumer
+#find_package is only for internal use
+find_dependency(manager_utils REQUIRED)
+
+if(NOT TARGET game_engine::game_engine)
+  include(${CMAKE_CURRENT_LIST_DIR}/game_engineTargets.cmake)
+endif()
+
+# This is for catkin compatibility.
+set(game_engine_LIBRARIES game_engine::game_engine)
+get_target_property(game_engine_INCLUDE_DIRS game_engine::game_engine INTERFACE_INCLUDE_DIRECTORIES)
+
