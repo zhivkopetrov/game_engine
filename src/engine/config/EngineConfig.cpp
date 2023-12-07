0ec3940d0d8d3a7894e6b631601cc955958f1679
Zhivko Petrov <zhivko.petrov@lucidlink.com>
2023-12-07 21:38:47 +0200

    chore: code tidy up

--- src/engine/config/EngineConfig.cpp
+++ src/engine/config/EngineConfig.cpp
@@ -14,11 +14,17 @@
 //Own components headers
 
 namespace {
-constexpr auto windowDisplayMode = WindowDisplayMode::FULL_SCREEN;
-constexpr auto windowBorderMode = WindowBorderMode::BORDERLESS;
-constexpr auto MAX_FRAME_RATE = 60;
+//Screen
+constexpr auto WINDOW_DISPLAY_MODE = WindowDisplayMode::FULL_SCREEN;
+constexpr auto WINDOW_BORDER_MODE = WindowBorderMode::BORDERLESS;
 constexpr auto MONITOR_WIDTH = 1920;
 constexpr auto MONITOR_HEIGHT = 1080;
+
+//Renderer
+constexpr auto RENDERER_FLAGS = 
+  getEnumValue(RendererFlag::HARDWARE_RENDERER) |
+  getEnumValue(RendererFlag::FBO_ENABLE);
+constexpr auto RENDERER_SCALE_QUALITY = RendererScaleQuality::LINEAR;
 constexpr auto MAX_RUNTIME_TEXTS = 500;
 constexpr auto MAX_RUNTIME_SPRITE_BUFFERS = 250;
 constexpr auto MAX_RUNTIME_IMAGES = 400;
@@ -29,13 +35,21 @@ constexpr auto MAX_RUNTIME_RENDERER_COMMANDS = MAX_RUNTIME_WIDGETS * 2;
 constexpr auto MAX_RENDERER_BACK_BUFFER_DATA_SIZE =
     std::numeric_limits<uint16_t>::max();
 
+
+//Engine
+constexpr auto MAX_FRAME_RATE = 60;
+
 #ifdef __EMSCRIPTEN__
-//emscripten port currently requires single-threaded usage on the
+//Emscripten port currently requires single-threaded usage on the
 //whole application level
 constexpr auto MAX_RESOURCE_LOADING_THREADS = 0;
 constexpr auto RENDERER_POLICY = RendererPolicy::SINGLE_THREADED;
 constexpr auto INPUT_EVENT_HANDLER_POLICY = 
   InputEventHandlerPolicy::POLL_BLOCKING;
+
+//Performance important note:
+//Emscripten does implement waiting on a conditional variable via busy CPU wait.
+//Don't wait on conditional variable in order not to throttle the CPU.
 constexpr auto ACTION_EVENT_HANDLER_POLICY =
   ActionEventHandlerPolicy::NON_BLOCKING;
 #else
@@ -80,14 +94,13 @@ EngineConfig getDefaultEngineConfig(
   monitorCfg.name = "Default Name";
   monitorCfg.width = MONITOR_WIDTH;
   monitorCfg.height = MONITOR_HEIGHT;
-  monitorCfg.displayMode = windowDisplayMode;
-  monitorCfg.borderMode = windowBorderMode;
+  monitorCfg.displayMode = WINDOW_DISPLAY_MODE;
+  monitorCfg.borderMode = WINDOW_BORDER_MODE;
 
   auto &rendererCfg = drawMgrCfg.rendererConfig;
   rendererCfg.executionPolicy = RENDERER_POLICY;
-  rendererCfg.flagsMask = getEnumValue(RendererFlag::HARDWARE_RENDERER) |
-      getEnumValue(RendererFlag::FBO_ENABLE);
-  rendererCfg.scaleQuality = RendererScaleQuality::LINEAR;
+  rendererCfg.flagsMask = RENDERER_FLAGS;
+  rendererCfg.scaleQuality = RENDERER_SCALE_QUALITY;
   rendererCfg.maxRuntimeRendererCommands = MAX_RUNTIME_RENDERER_COMMANDS;
   rendererCfg.maxRuntimeWidgets = MAX_RUNTIME_WIDGETS;
   rendererCfg.maxRendererBackBufferDataSize =
@@ -114,10 +127,11 @@ EngineConfig getDefaultEngineConfig(
 
 std::vector<DependencyDescription> getDefaultEngineDependencies(
     [[maybe_unused]]int32_t argc, [[maybe_unused]]char **args) {
-  const std::vector<DependencyDescription> dependencies { { "SDL2",
-      SDLLoader::initSdl2, SDLLoader::deinitSdl2 }, { "SDL2-image",
-      SDLLoader::initSdl2Image, SDLLoader::deinitSdl2Image }, { "SDL2-ttf",
-      SDLLoader::initSdl2Ttf, SDLLoader::deinitSdl2Ttf } };
+  const std::vector<DependencyDescription> dependencies {
+    { "SDL2", SDLLoader::initSdl2, SDLLoader::deinitSdl2 },
+    { "SDL2-image", SDLLoader::initSdl2Image, SDLLoader::deinitSdl2Image },
+    { "SDL2-ttf", SDLLoader::initSdl2Ttf, SDLLoader::deinitSdl2Ttf } 
+  };
 
   return dependencies;
 }
